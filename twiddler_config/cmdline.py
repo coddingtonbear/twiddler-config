97b63f9bbc73ec33dc03af3a20f46bc98b701357
Adam Coddington <me@adamcoddington.net>
2019-11-18 20:25:09 -0800

    Command-line bugfixes.

--- twiddler_config/cmdline.py
+++ twiddler_config/cmdline.py
@@ -5,17 +5,18 @@ import sys
 from .config import Config
 
 
-def main(*argv):
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
     parser = argparse.ArgumentParser()
     parser.add_argument('path', nargs=1, type=str)
 
     args = parser.parse_args(argv)
-    with open(os.path.expanduser(args.path[0]), 'rb') as inf:
-        cfg = Config.from_buffer(inf)
+    cfg = Config.from_path(args.path[0])
 
     for chord in cfg.chords:
         print(chord)
 
 
 if __name__ == '__main__':
-    main(*sys.argv[1:])
+    main()
